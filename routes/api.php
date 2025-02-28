53679d05624684c7a3dcdeb5a70c7d571f62e656
AmirAfsharPr <amirafshar.pr@gmail.com>
2025-02-28 19:23:38 +0330

    feat : handle forget and reset password process

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('auth')->group(function (){
     Route::post('/register',[AuthController::class,'register']);
     Route::post('/login',[AuthController::class,'login']);
+    Route::post('/forget-password',[AuthController::class,'forgetPassword']);
+    Route::post('/reset-password',[AuthController::class,'resetPassword']);
 });
 
 Route::prefix('app')->middleware('auth:api')->group(function (){
